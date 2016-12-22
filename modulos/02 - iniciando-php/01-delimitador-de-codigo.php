028172f34ee7de49c1fa94bf2fc7bfc3d922e2d2
Augusto Nascimento <augustoleite.nascimento@gmail.com>
2016-12-22 17:27:56 -0200

    Altaração feita

---
+++ modulos/02 - iniciando-php/01-delimitador-de-codigo.php
@@ -0,0 +1,23 @@
+<?php
+
+//comando para corrigir acentuação do código
+header('Content-Type: text/html; charset=utf-8');
+
+// exemplos de variáveis
+$ola = "Olá";
+$nome = "Augusto Nascimento";
+
+echo '$ola'; // aspas simples para exibir a variável
+echo "$ola"; // aspas duplas interpretar a variável
+
+// delimitar uma variável com chaves {} mais o resto de uma  string
+
+//exemplo
+echo '<hr>'; // quebra de linha
+echo "Olá Mundo meu nome é: {$nome}<br>";
+
+
+// outro comando de saída é o PRINT
+exemplo
+print("$nome");
+?>

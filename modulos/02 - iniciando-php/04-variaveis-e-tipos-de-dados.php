b6b1ff300050119684a54cd73954dc27fc48e00e
Augusto Nascimento <augustoleite.nascimento@gmail.com>
2016-12-23 11:43:10 -0200

    Trabalhando com Variáveis e tipos de dados

---
+++ modulos/02 - iniciando-php/04-variaveis-e-tipos-de-dados.php
@@ -0,0 +1,22 @@
+<?php
+
+//comando para corrigir acentuação do código
+header('Content-Type: text/html; charset=utf-8');
+
+// STR = String 
+//Exemplo:
+$str = "Olá Mundo";
+var_dump($str);
+
+echo "<hr>";
+
+//ARR = Array
+//Exemplo:
+
+$arr = array();
+    $arr['Nome'] = "Augusto Nascimento";
+    $arr['Numero'] = "Tenho 23 anos";
+
+var_dump($arr);
+
+echo "<hr>";

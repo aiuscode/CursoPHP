2c141547f75512e07e8364befae98bd2f84b380c
Augusto Nascimento <augustoleite.nascimento@gmail.com>
2016-12-23 11:06:08 -0200

    logotipo corrigida

--- modulos/02 - iniciando-php/01-delimitador-de-codigo.php
+++ modulos/02 - iniciando-php/01-delimitador-de-codigo.php
@@ -4,18 +4,28 @@
 header('Content-Type: text/html; charset=utf-8');
 
 // exemplos de variáveis
-$ola = "Olá";
+$ola = "Olá Mundo!";
 $nome = "Augusto Nascimento";
 
 echo '$ola'; // aspas simples para exibir a variável
 echo "$ola"; // aspas duplas interpretar a variável
-
 // delimitar uma variável com chaves {} mais o resto de uma  string
-
 //exemplo
 echo '<hr>'; // quebra de linha
 echo "Olá Mundo meu nome é: {$nome}<br>";
 
 
+print("Ola, {$nome}");
 
+$Arr = array(
+        "Ola, $nome, tudo bem? . é programador",
+        "Ola" => "teste"
+        );
+print_r($Arr);
+echo "<hr>";
 ?>
+
+    <article>
+             <h1><?= $ola;?></h1>
+             <p>Meu nome é <?php echo $nome; ?></p>
+</article>
